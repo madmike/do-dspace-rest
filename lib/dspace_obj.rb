7b8dfbdd5e0460a32713542a831195d9cf0f7e99
Monika Mevenkamp <monikam@princeton.edu>
2015-09-14 13:27:40 +0200

    test listers of dspace rest api

---
+++ lib/dspace_obj.rb
@@ -0,0 +1,56 @@
+require 'dspace_rest'
+
+class DSpaceObj
+
+  def initialize(hsh = {})
+    @attributes = hsh
+  end
+
+  def save
+    return App::REST_API.post(self.class::PATH, @attributes)
+  end
+
+  def delete
+    return App::REST_API.delete(self.class::PATH + "/#{@attributes['id']}")
+  end
+
+  def attributes
+    return @attributes
+  end
+
+  def []=(key,value)
+    @attributes[key] = value
+    return value
+  end
+
+  def [](key)
+    @attributes[key]
+  end
+
+  def self.get_list(path, klass, params)
+    l = []
+    App::REST_API.get(path, params).each do |c|
+      obj = klass.new()
+      parse(obj, c)
+      l << obj
+    end
+    return l
+  end
+
+  def self.get_one(path, klass)
+    raw_json =  App::REST_API.get(path, {})
+    obj = klass.new()
+    parse(obj, raw_json)
+    return obj
+  end
+
+  def self.parse(object, raw_json)
+    raw_json.each do |key, value|
+      unless key == 'expand'
+        object[key] =  value
+      end
+    end
+  end
+
+end
+
